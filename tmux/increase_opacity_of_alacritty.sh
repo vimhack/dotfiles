aa54aee3fcfe8f12a89d6e0c7f53d3e37dc60ebe
windvalley <i@sre.im>
2021-07-25 13:41:59 +0800

    feat(tmux): add hot key to increase and decrease the opacity of Alacritty
    prefix + ctrl k: decrease opacity of alacritty;
    prefix + ctrl j: increase opacity of alacritty.

---
+++ tmux/increase_opacity_of_alacritty.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env zsh
+# increase_opacity_of_alacritty.sh
+#
+
+dotfiles_dir=~/.dotfiles
+alacritty_conf=~/.config/alacritty/alacritty.yml
+step=0.05
+
+[[ ! -f "$alacritty_conf" ]] && {
+    \cp $dotfiles_dir/alacritty/alacritty.yml $alacritty_conf
+}
+
+value=$(awk '/^background_opacity/{print $NF}' $alacritty_conf)
+
+to_value=0$(echo "scale=2;$value+$step" | bc)
+
+[[ $to_value -gt 1 ]] && exit 0
+
+current_color=$(awk -F'*' '/^colors:/{print $2}' $alacritty_conf)
+current_font_size=$(awk -F: '/  size:/{print $2}' $alacritty_conf)
+
+\cp $dotfiles_dir/alacritty/alacritty.yml $alacritty_conf
+gsed -i "/^colors/s/^.*$/colors: *$current_color/" $alacritty_conf
+gsed -i "/^  size:/s/^.*$/  size: $current_font_size/" $alacritty_conf
+
+gsed -i "/^background_opacity/s/^.*$/background_opacity: $to_value/" $alacritty_conf
+
+exit 0
