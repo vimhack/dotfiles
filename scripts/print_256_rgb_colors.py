c00d21e0c26a340a2a8d4bec22de7097a72ae158
windvalley <i@sre.im>
2021-07-30 15:17:17 +0800

    refactor(zsh): optimize command 'rgb'

--- scripts/print_256_rgb_colors.py
+++ scripts/print_256_rgb_colors.py
@@ -15,7 +15,7 @@ import subprocess
 def get_width(default=80):
     """Attempt to detect console width and default to 80"""
     try:
-        columns, rows = shutil.get_terminal_size()
+        columns, _ = shutil.get_terminal_size()
     except AttributeError:
         try:
             _, columns = subprocess.check_output(["stty", "size"]).split()
@@ -37,38 +37,38 @@ colored_palette = [(r, g, b) for r in colored for g in colored for b in colored]
 grayscale_palette = [(g, g, g) for g in range(8, 240, 10)]
 
 
-esc = "\033["
+ESC = "\033["
 # Reset all colors sequence
-reset = esc + "0m"
+RESET = ESC + "0m"
 # Regular color
-normal = esc + "38;5;{i}m"
+NORMAL = ESC + "38;5;{i}m"
 # Bold color
-bold = esc + "1;" + normal
+BOLD = ESC + "1;" + NORMAL
 # Background color
-background = esc + "48;5;{i}m"
+BACKGROUND = ESC + "48;5;{i}m"
 
-pattern = (
-    "{normal}{background}{padding:^{width}}{i:^3d} "  # pad the background
+PATTERN = (
+    "{NORMAL}{BACKGROUND}{padding:^{width}}{i:^3d} "  # pad the background
     "{r:02X}/{g:02X}/{b:02X}"  # show the hex rgb code
     "{padding:^{width}}"  # pad the background on the other side
-    "{reset}"  # reset again
+    "{RESET}"  # reset again
 )
 
-base_context = dict(reset=reset, padding="", width=get_width())
+base_context = dict(RESET=RESET, padding="", width=get_width())
 
 for i, (r, g, b) in enumerate(colored_palette + grayscale_palette, 16):
     context = dict(i=i, r=r, g=g, b=b, color=r + g + b, **base_context)
-    context.update(bold=bold.format(**context))
-    context.update(background=background.format(**context))
+    context.update(BOLD=BOLD.format(**context))
+    context.update(BACKGROUND=BACKGROUND.format(**context))
 
     # Change text color from black to white when it might become unreadable
 
     if max(r, g, b) > 0xCC:
-        context.update(normal=normal.format(i=0))
+        context.update(NORMAL=NORMAL.format(i=0))
     else:
-        context.update(normal=normal.format(i=255))
+        context.update(NORMAL=NORMAL.format(i=255))
 
-    print(pattern.format(**context), end="")
+    print(PATTERN.format(**context), end="")
 
     # Print newlines when needed
 
